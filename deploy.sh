21718c79ed728a09785c9ec484a9356cf9edb2d4
Mike iLL Kilmer <mike@mzoo.org>
2015-12-01 09:38:23 -0600

    Initial commit.

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rsync -avP css img *.php vr:/home5/viewingr/public_html/rebeccaturner/D6MpoCeVseClFO5fkCMZAqNS
---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Rebecca Turner - Editing, Copywriting, Content Strategy & Development</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div id="container">
+
+           <!-- header -->
+           <header id="header">
+              <h1 id="title">Rebecca Turner</h1>
+              <h2 class="tagline">Editing, Copywriting, Content Strategy & Development</h2>
+           </header>
+
+
+           <!-- Main Content area -->
+           <section id="content">
+              <h2>Welcome and thank you for the consideration.</h2>
+              <!-- Main content -->
+              <p>Please click below to view PDF files of my resume and writing samples.</p>
+              <p>You may also right-click to download them to your device.</p>
+
+              <div class="files">Rebecca Turner Resume</div>
+
+              <div class="files">
+                <ul>
+                    <li>A writing sample</li>
+                    <li>Another writing sample</li>
+                    <li>A third </li>
+                    <li>And fourth writing sample</li>
+                </ul>
+              </div>
+           </section>
+
+           <!-- Sidebar -->
+           <aside id="sidebar">
+              <!-- Sidebar content -->
+              <h2>Contact Information</h2>
+              Email: <a href="mailto:rebeccamo34@gmail.com">rebeccamo34@gmail.com</a>
+              <a href="https://www.linkedin.com/in/rebecca-turner-8200685" target="_blank"><img src="img/Logo-2C-28px-TM.png"/></a>
+           </aside>
+
+           <!-- Footer -->
+           <footer id="footer" class="clearfix">
+              Copyright &copy Rebecca Turner 2015
+           </footer>
+
+        </div>
+
+    </body>
+</html>
