666806f9134213bca32bc36c10ce5dc0960bde16
Mike iLL Kilmer <mike@mzoo.org>
2016-09-07 10:20:57 -0500

    Add image and div.

--- index.php
+++ index.php
@@ -31,10 +31,8 @@
            <!-- Main Content area -->
            <section id="content"><p><p>
               <h4>"I'm a lover of words with many years of experience writing clear, concise, and charismatic copy. From bold marketing banners, to ecommerce copy that makes the sale, to the all-important user guidance within an app, I've done it all. I'm unabashedly in favor of quality writing (and correct grammar), yet can still grab the reader's attention with economy and precision."
-<p><p><p><p>
               <!-- Main content -->
-<p></P>
-            
+
 
               <div class="files">
                 <ol>
@@ -44,8 +42,11 @@
 
               <div class="files">
                 <p><h2>Portfolio</h2></P><br>
-<h3>Marketing copy</h3><p>
+                <h3>Marketing copy</h3><p>
                 <h4>"This is no act..."</h4>
+                <div class="campaign">
+                <div><img src="samples/babybook.png" /></div>
+                <div>
                 <p><h5>When Weight Watchers introduced Jennifer Hudson as a spokesperson, I worked with marketing staff and designers to come up with bold headline copy befitting the star's new look.</h5></p>
                 <ol>
                     <p><a href="samples/homepage_jen.pdf" target="_blank">Introducing Jennifer Hudson</a></p> </ol><br>
@@ -54,6 +55,8 @@
                 <p><h5>I was asked to help update the language in this guide that new parents get when they register. The tone had to be more fun and friendly. On this page the headline had previously been "Not your grandma's diapers." I changed it to "Diapers, from top to bottom." </h5></p>
                 <ol>
                     <p><a href="samples/babybook.png" target="_blank">Baby Book</a></p> </ol><br>
+                </div>
+                </div>
 
                 <h4>buybuy BABY "About Us" rewrite</h4>
                 <p><h5>I was asked to bring this very old "About Us" page in line with updates to our stores and site, using a more friendly brand voice.</h5></p>
@@ -69,6 +72,7 @@
 <br>
 <h3>Longform copy</h3><p>
  <h4>Introducing Weight Watchers</h4>
+
                 <p><h5>One of my most rewarding tasks at Weight Watchers was introducing members to the Weight Watchers plan. This was a highly collaborative process in which I worked with our design, program, science, and legal teams.</h5></p>
                 <ol>
                     <p><a href="samples/planguides_intro.pdf" target="_blank">Weight Watchers plan guide</a></p>
@@ -86,17 +90,9 @@
                     <p><a href="samples/boho.jpg" target="_blank">Girls' trends</a></p></li>
                     <p><a href="samples/casual.jpg" target="_blank">Boys' trends</a></p>
                 </ol><br>
-
-               
-
+</div>
 
 
-                
-
-
-              </div>
-
-              
            </section>
 
            <!-- Sidebar -->
