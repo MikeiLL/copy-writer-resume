0def7c06bfdec509b39bc8702fe731807fdfc0ee
Mike iLL Kilmer <mike@mzoo.org>
2016-09-07 11:23:08 -0500

    Add some thumbnails.

--- index.php
+++ index.php
@@ -1,114 +1,125 @@
 <!doctype html>
 <html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Rebecca Turner: Copywriting, Content Development and Strategy</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Rebecca Turner: Copywriting, Content Development and Strategy</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Rufina" />
-    </head>
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        <div id="container">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Rufina" />
+</head>
+<body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <!--[if !IE 7]>
+        <style type="text/css">
+            #wrap {display:table;height:100%}
+        </style>
+    <![endif]-->
+    <div id="container">
 
-           <!-- header -->
-           <header id="header">
-              <h1 id="title">Rebecca Turner</h1>
-              <h2 class="tagline">Senior Copywriter</h2>
-           </header>
+       <!-- header -->
+       <header id="header">
+          <h1 id="title">Rebecca Turner</h1>
+          <h2 class="tagline">Senior Copywriter</h2>
+       </header>
 
 
-           <!-- Main Content area -->
-           <section id="content"><p><p>
-              <h4>"I'm a lover of words with many years of experience writing clear, concise, and charismatic copy. From bold marketing banners, to ecommerce copy that makes the sale, to the all-important user guidance within an app, I've done it all. I'm unabashedly in favor of quality writing (and correct grammar), yet can still grab the reader's attention with economy and precision."
-              <!-- Main content -->
+       <!-- Main Content area -->
+       <section id="content"><p><p>
+          &ldquo;I'm a lover of words with many years of experience writing clear, concise, and charismatic copy. From bold marketing banners, to ecommerce copy that makes the sale, to the all-important user guidance within an app, I've done it all. I'm unabashedly in favor of quality writing (and correct grammar), yet can still grab the reader's attention with economy and precision.&rdquo;
+          <!-- Main content -->
 
 
-              <div class="files">
-                <ol>
-                    <p><a href="Rebecca_Turner_2016.docx" target="_blank">My resume</a></P>
-                </ol><br>
-             </div>
+          <div class="files">
+            <ol>
+                <p><a href="Rebecca_Turner_2016.docx" target="_blank">My resume</a></P>
+            </ol><br>
+         </div>
 
-              <div class="files">
-                <p><h2>Portfolio</h2></P><br>
-                <h3>Marketing copy</h3><p>
-                <h4>"This is no act..."</h4>
-                <div class="campaign">
-                <div><img src="samples/babybook.png" /></div>
+          <div class="files">
+            <p><h2>Portfolio</h2></P><br>
+            <h3>Marketing copy</h3><p>
+            <h4>"This is no act..."</h4>
+            <div class="campaign">
                 <div>
-                <p><h5>When Weight Watchers introduced Jennifer Hudson as a spokesperson, I worked with marketing staff and designers to come up with bold headline copy befitting the star's new look.</h5></p>
-                <ol>
-                    <p><a href="samples/homepage_jen.pdf" target="_blank">Introducing Jennifer Hudson</a></p> </ol><br>
-
-<h4>buybuy BABY Baby Book rewrite</h4>
-                <p><h5>I was asked to help update the language in this guide that new parents get when they register. The tone had to be more fun and friendly. On this page the headline had previously been "Not your grandma's diapers." I changed it to "Diapers, from top to bottom." </h5></p>
-                <ol>
-                    <p><a href="samples/babybook.png" target="_blank">Baby Book</a></p> </ol><br>
+                    <a href="samples/homepage_jen.pdf" title="View as PDF" target="_blank"><img src="img/thumbnails/homepage_jen.jpg" /></a>
                 </div>
+                <div>
+                    <p>When Weight Watchers introduced Jennifer Hudson as a spokesperson, I worked with marketing staff and designers to come up with bold headline copy befitting the star's new look.</p>
+                    <ol><p><a href="samples/homepage_jen.pdf" title="View as PDF" target="_blank">Introducing Jennifer Hudson</a></p> </ol>
                 </div>
+            </div>
+            <div class="campaign">
+                <div>
+                    <a href="samples/babybook.png" title="View as PDF" target="_blank"><img src="img/thumbnails/babybook.png" /></a>
+                </div>
+                <div>
+                <h4>buybuy BABY Baby Book rewrite</h4>
+                    <p>I was asked to help update the language in this guide that new parents get when they register. The tone had to be more fun and friendly. On this page the headline had previously been "Not your grandma's diapers." I changed it to "Diapers, from top to bottom." </p>
+                    <ol><a href="samples/babybook.png" title="View as PDF" target="_blank">Baby Book</a></ol>
+                </div>
+            </div>
 
-                <h4>buybuy BABY "About Us" rewrite</h4>
-                <p><h5>I was asked to bring this very old "About Us" page in line with updates to our stores and site, using a more friendly brand voice.</h5></p>
-                <ol>
+            <h4>buybuy BABY "About Us" rewrite</h4>
+            <p>I was asked to bring this very old "About Us" page in line with updates to our stores and site, using a more friendly brand voice.</p>
+            <ol>
 <p><a href="samples/Who We Are.pdf" target="_blank">About Us</a></p></ol><br>
-                <p>
+            <p>
 <h4>Understanding the options</h4>
-                <p><h5>Our merchandising team tasked me with writing an interactive banner to help our customers sort through the different types of swaddles available.</h5></p>
-                <ol>
-                    <p><a href="samples/swaddle2.gif" target="_blank">Swaddle intro</a></p>
+            <p>Our merchandising team tasked me with writing an interactive banner to help our customers sort through the different types of swaddles available.</p>
+            <ol>
+                <p><a href="samples/swaddle2.gif" target="_blank">Swaddle intro</a></p>
 <p><a href="samples/swaddle1.gif" target="_blank">Wearable blankets</a></p>
- </ol>
+</ol>
 <br>
 <h3>Longform copy</h3><p>
- <h4>Introducing Weight Watchers</h4>
+<h4>Introducing Weight Watchers</h4>
 
-                <p><h5>One of my most rewarding tasks at Weight Watchers was introducing members to the Weight Watchers plan. This was a highly collaborative process in which I worked with our design, program, science, and legal teams.</h5></p>
-                <ol>
-                    <p><a href="samples/planguides_intro.pdf" target="_blank">Weight Watchers plan guide</a></p>
-                </ol><br>
+            <p>One of my most rewarding tasks at Weight Watchers was introducing members to the Weight Watchers plan. This was a highly collaborative process in which I worked with our design, program, science, and legal teams.</p>
+            <ol>
+                <p><a href="samples/planguides_intro.pdf" target="_blank">Weight Watchers plan guide</a></p>
+            </ol><br>
 
-                <h4>Blogging on all things Weight Watchers</h4>
-                <p><h5>I had so much fun musing on food, eating, activity, and more for the WeightWatchers.com editor's blog!</h5></p>
-                <ol>
-                    <p><a href="http://community.weightwatchers.com/Blogs/SearchBlogs.aspx?s=rebecca+turner&t=16&sh=0&" target="_blank">Weight Watchers blogs</a></p>
-                </ol><br>
+            <h4>Blogging on all things Weight Watchers</h4>
+            <p>I had so much fun musing on food, eating, activity, and more for the WeightWatchers.com editor's blog!</p>
+            <ol>
+                <p><a href="http://community.weightwatchers.com/Blogs/SearchBlogs.aspx?s=rebecca+turner&t=16&sh=0&" target="_blank">Weight Watchers blogs</a></p>
+            </ol><br>
 <h3>Ecommerce copy</h3><p>
 <h4>Get the look</h4>
-                <p><h5>BuyBuyBaby.com apparel buyers created kids' clothes collections based on styles and looks. I named the collections and wrote the copy for these fun and successful collections.</h5></p>
-                <ol>
-                    <p><a href="samples/boho.jpg" target="_blank">Girls' trends</a></p></li>
-                    <p><a href="samples/casual.jpg" target="_blank">Boys' trends</a></p>
-                </ol><br>
+            <p>BuyBuyBaby.com apparel buyers created kids' clothes collections based on styles and looks. I named the collections and wrote the copy for these fun and successful collections.</p>
+            <ol>
+                <p><a href="samples/boho.jpg" target="_blank">Girls' trends</a></p></li>
+                <p><a href="samples/casual.jpg" target="_blank">Boys' trends</a></p>
+            </ol><br>
 </div>
 
 
-           </section>
+       </section>
 
-           <!-- Sidebar -->
-           <aside id="sidebar">
-              <!-- Sidebar content -->
-              <h2>Contact Information</h2>
-              Email: rebeccamo34@gmail.com
-              <a href="https://www.linkedin.com/in/rebecca-turner-8200685" target="_blank"><img src="img/Logo-2C-28px-TM.png"/></a>
-           </aside>
+       <!-- Sidebar -->
+       <aside id="sidebar">
+          <!-- Sidebar content -->
+          <h2>Contact Information</h2>
+          Email: rebeccamo34@gmail.com
+          <a href="https://www.linkedin.com/in/rebecca-turner-8200685" target="_blank"><img src="img/Logo-2C-28px-TM.png"/></a>
+       </aside>
 
-           <!-- Footer -->
-           <footer id="footer" class="clearfix">
-              Copyright &copy Rebecca Turner 2016
-           </footer>
+       <!-- Footer -->
+       <footer id="footer" class="clearfix">
+          Copyright &copy Rebecca Turner 2016
+       </footer>
 
-        </div>
+    </div>
 
-    </body>
+</body>
 </html>
